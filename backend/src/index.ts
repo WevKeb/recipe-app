f966c941629b6b1446da0b0a1144ca06c598687c
Kevin Weber <weberkm5@gmail.com>
2024-03-04 22:29:12 -0600

    first commit, stopped at 1hr 10min

---
+++ backend/src/index.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import * as RecipeAPI from './recipe-api';
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/api/recipes/search', async (req, res)=>{
+    // GET http://localhost/api/recipes/search?searchTerm=burgers&page=1
+    
+    const searchTerm = req.query.searchTerm as string;
+    const page = parseInt(req.query.page as string);
+    const results = await RecipeAPI.searchRecipes(searchTerm, page);
+
+    return res.json(results);
+});
+
+app.listen(5000, ()=>{
+    console.log('server is running on localhost:5000 yeah baby!!');
+})
